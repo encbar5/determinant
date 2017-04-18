060ae7035d4d8be6f120d52b47df0e6f05e2f334
Landon Sherwood <landonsherwood23@gmail.com>
2017-04-18 16:07:54 -0500

    added zero determinant

--- src/readMatrix.f
+++ src/readMatrix.f
@@ -1,10 +1,10 @@
       module readMatrix
 
       implicit none
-       
+
       ! Define interface
       interface readMatrixIface
-           module procedure readM, getRandM
+           module procedure readM, getRandM, getZeroM
       end interface readMatrixIface
 
       contains
@@ -34,5 +34,42 @@
               call random_number(Mat)
               Mat = Mat - 0.5d+0
       end subroutine getRandM
+
+      subroutine getZeroM(m,Mat)
+        implicit none
+        integer, intent(in) :: n,m
+        integer :: k,i,j,l,p
+        double precision, intent(out) :: Mat(n,m)
+        integer, allocatable :: seed(:)
+        integer :: randsize
+        real :: r
+
+        call random_seed(size=randsize)
+        allocate(seed(randsize))
+        OPEN(89,FILE='/dev/urandom',ACCESS='stream',FORM='UNFORMATTED')
+        READ(89) seed
+        CLOSE(89)
+        call random_seed(put=seed)
+        call random_number(Mat)
+        Mat = Mat - 0.5d+0
+
+        ! Get a random number
+        call random_number(r)
+        i = floor( r*n )
+
+        ! Get another
+        call random_number(r)
+        j = floor( r*n )
+
+        ! Copy a row
+        do k = 1,5
+          Mat(i,k) = Mat(j,k)
+        end do
+
+        ! Output matrix to debug
+        !do l = 1,5
+        !    Print *, (Mat(l,p), p=1,5)
+        !end do
+      end subroutine getZeroM
 
       end module readMatrix
